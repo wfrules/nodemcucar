575ca74c3b772d1c25348f50829fdf8283fb1296
wf <wf@mlf.com>
2020-05-16 23:14:22 +0800

    Signed-off-by: wf <wf@mlf.com>

--- proj/mbutton.lua
+++ proj/mbutton.lua
@@ -1,15 +1,20 @@
 require("mdebug")
+require("mwifi")
 mButton = {}
 mButton.gpio = 3
 function mButton.watch()
     gpio.mode(mButton.gpio, gpio.INPUT)
-    while(true)
+    bLoop = true
+    while(bLoop)
     do
         tmr.wdclr()
         flag = gpio.read(mButton.gpio)
-        bOn = mDebug.transFlag(flag)
-        mDebug.turnLight(bOn)
-        
+        if flag == 0 then
+            tmr.delay(1000 * 1000)
+            mDebug.blink()
+            mWifi.conn()
+            bLoop = false
+        end    
     end    
 end
 
